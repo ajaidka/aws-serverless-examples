fc0bbc30b6d05ac56e2ab11ba995895b344fd96c
aashu jaidka <aashu.jaidka@paxcel.net>
2018-04-12 13:48:54 +0530

    added sample API for get request workflow

--- node-weather-cron/handler.js
+++ node-weather-cron/handler.js
@@ -8,14 +8,14 @@ module.exports.collectWeatherData = (event, context, callback) => {
   https.get(weatherAPI, (resp) => {
     let data = '';
 
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
+    // A dataChunk of data has been recieved.
+    resp.on('data', (dataChunk) => {
+      data += dataChunk;
     });
 
     // The whole response has been received. Print out the result.
     resp.on('end', () => {
-      console.log(JSON.parse(data).explanation);
+      console.log(JSON.parse(data));
       const response = {
         statusCode: 200,
         body: JSON.stringify({
@@ -24,11 +24,11 @@ module.exports.collectWeatherData = (event, context, callback) => {
           input: event,
         }),
       };
-    
+
       callback(null, response);
     });
 
-    
+
 
   }).on("error", (err) => {
     console.log("Error: " + err.message);
